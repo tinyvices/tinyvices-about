6d2d2179bfaf8e38ba3e14f9bddb80564b006a82
tinyvices <tinyvices@tinyvices.io>
2019-08-04 16:44:14 -0400

    not coming soon

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">tinyvices.io</h1>
         </header>
         <p className="App-intro">
-	    Coming Soon
+	    ¯
         </p>
       </div>
     );
